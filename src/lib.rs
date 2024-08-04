5de19b4b0432dc786d6024f1febc743438d43804
insolor <2442833+insolor@users.noreply.github.com>
2024-08-04 15:54:13 +0300

    Move PyMorphAnalyzer into py_classes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use pyo3::prelude::*;
-mod analyzer;
 mod converters;
 mod py_classes;
 
 #[pymodule]
 fn morphrs_py(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_class::<analyzer::PyMorphAnalyzer>()?;
+    m.add_class::<py_classes::PyMorphAnalyzer>()?;
     Ok(())
 }
--- src/py_classes.rs
+++ src/py_classes.rs
@@ -1,4 +1,32 @@
-use pyo3::prelude::*;
+use morph_rs::MorphAnalyzer;
+use pyo3::{prelude::*, types::PyList};
+use std::path::Path;
+
+use crate::converters::convert_parsed_words;
+
+#[pyclass]
+#[pyo3(name = "MorphAnalyzer", subclass)]
+pub struct PyMorphAnalyzer {
+    morph_analyzer: MorphAnalyzer,
+}
+
+#[pymethods]
+impl PyMorphAnalyzer {
+    #[staticmethod]
+    fn open(path: String) -> PyResult<Self> {
+        let morph_analyzer = MorphAnalyzer::open(Path::new(&path)).unwrap();
+        Ok(PyMorphAnalyzer { morph_analyzer })
+    }
+
+    fn parse_word(&self, word: String) -> Py<PyList> {
+        let parsed_words = self.morph_analyzer.parse_word(&word).unwrap();
+        convert_parsed_words(parsed_words).unwrap()
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok("MorphAnalyzer()".to_string())
+    }
+}
 
 #[pyclass]
 #[pyo3(name = "ParsedWord", subclass)]
